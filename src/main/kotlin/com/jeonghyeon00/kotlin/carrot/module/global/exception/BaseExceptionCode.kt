75a3b6392a6aef848078e5653e8574d0ce560de9
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-06-21 17:02:02 +0900

    feat: JWT 토큰에서 ID 가져오는 어노테이션 구현

--- src/main/kotlin/com/jeonghyeon00/kotlin/carrot/module/global/exception/BaseExceptionCode.kt
+++ src/main/kotlin/com/jeonghyeon00/kotlin/carrot/module/global/exception/BaseExceptionCode.kt
@@ -6,6 +6,7 @@ enum class BaseExceptionCode(val httpStatusCode: Int, val message: String) {
     // 400 BAD REQUEST
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다."),
     REFRESH_TOKEN_MISMATCH(HttpStatus.BAD_REQUEST.value(), "리프레쉬 토큰이 일치하지 않습니다."),
+    AUTHORIZATION_HEADER_NULL(HttpStatus.BAD_REQUEST.value(), "인증 헤더가 null입니다."),
 
     // 409 CONFLICT
     USER_ID_CONFLICT(HttpStatus.CONFLICT.value(), "아이디가 중복입니다."),
