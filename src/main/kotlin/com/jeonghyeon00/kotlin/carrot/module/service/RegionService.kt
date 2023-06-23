e64e86f77b2df95c545f1e4fc915efa271aa02d9
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-06-23 14:02:24 +0900

    feat: 지역 추가 예외처리 추가

--- src/main/kotlin/com/jeonghyeon00/kotlin/carrot/module/service/RegionService.kt
+++ src/main/kotlin/com/jeonghyeon00/kotlin/carrot/module/service/RegionService.kt
@@ -1,5 +1,7 @@
 package com.jeonghyeon00.kotlin.carrot.module.service
 
+import com.jeonghyeon00.kotlin.carrot.module.global.exception.BaseException
+import com.jeonghyeon00.kotlin.carrot.module.global.exception.BaseExceptionCode
 import com.jeonghyeon00.kotlin.carrot.module.repository.RegionRepository
 import com.jeonghyeon00.kotlin.carrot.module.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -11,7 +13,11 @@ class RegionService(private val regionRepository: RegionRepository, private val 
     fun addRegion(userId: String, regionId: Long): Boolean {
         val user = userRepository.getReferenceById(userId)
         val region = regionRepository.getReferenceById(regionId)
-        user.regions.add(region)
+        if (!user.regions.contains(region) && user.regions.count() < 2) {
+            user.regions.add(region)
+        } else {
+            throw BaseException(BaseExceptionCode.DUPLICATE_REGION_OR_OVER_TWO_REGIONS)
+        }
         return true
     }
 }
