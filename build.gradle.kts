63f3a2fa5b0153a1bc9d90a1a9435a9a3919878b
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-07-11 17:04:44 +0900

    chore: livereload 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     runtimeOnly("org.postgresql:postgresql")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("io.jsonwebtoken", "jjwt-api", "0.11.2")
     runtimeOnly("io.jsonwebtoken", "jjwt-impl", "0.11.2")
     runtimeOnly("io.jsonwebtoken", "jjwt-jackson", "0.11.2")
